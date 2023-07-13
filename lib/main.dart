946e99abbdd42db557b3fc7c05d28468314d9579
Aditya Tundwal <aditya_tundwal_@outlook.com>
2023-07-14 00:06:28 +0530

    added other file

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,16 @@ class TestApp extends StatelessWidget {
   const TestApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    int days = 30;
+    double days1 = 300.0;
+    String name = "Aditya";
+    bool isMale = true;
+
     return MaterialApp(
       home: Material(
         child: Center(
           child: Container(
-            child: Text("Welcome to the TestApp"),
+            child: Text("Welcome to the TestApp $name $days $days1"),
           ),
         ),
       ),
